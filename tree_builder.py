d972b4d61dd6d819f7265e9ed5f652c7cf02bf69
tigersan <32451363+tigersan@users.noreply.github.com>
2025-03-08 11:52:25 +0100

    Update tree_builder.py

--- tree_builder.py
+++ tree_builder.py
@@ -25,29 +25,29 @@ Update to the tree_builder.py file to fix the initialization issue
 
 # At the top of tree_builder.py, update the __init__ method:
 
-def __init__(self, db, scraper=None):
-    """
-    Initialize the tree builder.
-    
+    def __init__(self, db, scraper=None):
+        """
+        Initialize the tree builder.
+        
         Args:
-        db: Database instance for storing and retrieving data
-        scraper: Optional scraper for searching additional records
-    """
-    self.db = db
-    self.scraper = scraper
-    # Gender constants
-    self.MALE = 'M'
-    self.FEMALE = 'F'
-    self.UNKNOWN = 'U'
-    # Tracking for recursive searches
-    self.search_queue = []
-    self.searched_persons = set()
-    self.stats = {
-        "persons_found": 0,
-        "generations_searched": 0,
-        "searches_performed": 0,
-        "extended_family_members": 0
-    }
+            db: Database instance for storing and retrieving data
+            scraper: Optional scraper for searching additional records
+        """
+        self.db = db
+        self.scraper = scraper
+        # Gender constants
+        self.MALE = 'M'
+        self.FEMALE = 'F'
+        self.UNKNOWN = 'U'
+        # Tracking for recursive searches
+        self.search_queue = []
+        self.searched_persons = set()
+        self.stats = {
+            "persons_found": 0,
+            "generations_searched": 0,
+            "searches_performed": 0,
+            "extended_family_members": 0
+        }
     
     def import_scraped_data(self, data):
         """
